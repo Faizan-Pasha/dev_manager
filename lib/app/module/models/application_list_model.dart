8051a3c0eace29ff1f82df07a854582c7aaf8ce0
Faizan Pasha <faizanpasha265@gmail.com>
2023-09-20 20:49:16 +0530

    Removed Riverpod Dependencies

--- lib/app/module/models/application_list_model.dart
+++
@@ -1,7 +0,0 @@
-class ApplicationListModel {
-  bool status;
-  bool error;
-  String? errorMessage;
-  ApplicationListModel(
-      {required this.status, required this.error, this.errorMessage});
-}
